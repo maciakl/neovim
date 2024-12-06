da78e55449610b659873ac545dcdc9aa483477cd
Luke Maciak <luke@maciak.net>
2024-12-06 00:37:58 -0500

    Added help popup

--- init.lua
+++ init.lua
@@ -91,3 +91,5 @@ end
 if vim.g.neovide then
     require('neovide');
 end
+
+require('hpop')
---
+++ lua/hpop.lua
@@ -0,0 +1,62 @@
+-- HELP POPUP
+-- A reminder popup for my custom key bindings
+
+local popup = require("plenary.popup")
+local Win_id
+
+-- Show a popup window with the given list of strings
+function ShowPop(opts)
+  local height = 20
+  local width = 40
+  local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+  Win_id = popup.create(opts, {
+        title = "Help",
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+  })
+  local bufnr = vim.api.nvim_win_get_buf(Win_id)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua ClosePop()<CR>", { silent=false })
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<esc>", "<cmd>lua ClosePop()<CR>", { silent=false })
+end
+
+-- Close the popup window
+function ClosePop()
+  vim.api.nvim_win_close(Win_id, true)
+end
+
+
+-- Show my custom key bindings and shortcuts I forget
+function Helpme()
+    ShowPop({
+            " jj            ➡ <ESC>",
+            " <ld><spc>     ➡ clear search",
+            " <ld>y         ➡ system clipboard",
+            " <C-l>         ➡ fix spelling",
+            " <A-left>      ➡ prev buff",
+            " <A-right>     ➡ next buff",
+            " <A-up>        ➡ search buffers",
+            " <A-down>      ➡ search files",
+            " <A-\\>         ➡ lsp search doc symbols",
+            " <C-\\>         ➡ lsp search proj symbols",
+            " <F7>          ➡ harpoon show",
+            " <F6>          ➡ harpoon add",
+            " <A-S-left>    ➡ harpoon prev",
+            " <A-S-right>   ➡ harpoon next",
+            " <ld>fd        ➡ lsp def",
+            " <ld>fg        ➡ live grep",
+            " <S-j>         ➡ move selection down",
+            " <S-k>         ➡ move selection up",
+            " ---",
+            " <C-w>v        ➡ split",
+            " <C-w>w        ➡ switch split",
+            " <C-w>c        ➡ close split",
+
+    })
+end
+
+-- Bind the help popup to <leader>h
+vim.cmd([[
+    nnoremap <leader>h :lua Helpme()<CR>
+]])

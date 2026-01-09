769c40212b3862e0356f8fe4f5bd89f7e4f6e88f
Luke Maciak <luke@maciak.net>
2026-01-08 20:19:56 -0500

    fix the tree-sitter config for the new version

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -2,7 +2,7 @@
 -------------------
 -- These are Treesitter specific settings
 
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter.config'.setup {
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
 	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go" },
 

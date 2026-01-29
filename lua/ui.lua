1dfec3d82cae4429e5b60f5727eba6319dee9071
Luke Maciak <luke@maciak.net>
2026-01-29 18:46:56 -0500

    fix spacing on buffline

--- lua/ui.lua
+++ lua/ui.lua
@@ -14,7 +14,7 @@ require('bufferline').setup{
         separator_style = "slant",
         diagnostics = "nvim_lsp",
         diagnostics_indicator = function(count, level)
-            local icon = level:match("error") and "" or ""
+            local icon = level:match("error") and " " or " "
             return " " .. icon .. count
         end,
     }

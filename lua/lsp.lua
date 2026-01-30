2111d0aa2e408e4df821b589e9080770cd0e3f23
Luke Maciak <luke@maciak.net>
2026-01-29 22:44:00 -0500

    Added bindings for error browsing

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -13,6 +13,9 @@ vim.diagnostic.config({ virtual_text = true })
 vim.keymap.set('n', '<A-i>', '<cmd>lua vim.lsp.buf.hover()<cr>')
 vim.keymap.set('n', '<A-o>', '<cmd>lua vim.diagnostic.open_float()<cr>')
 
+vim.keymap.set('n', '<A-n>', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+vim.keymap.set('n', '<A-p>', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
 -- gd to go to definition <ld>gd show definition in a split
 vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
 vim.keymap.set('n', '<leader>gd', '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>')
